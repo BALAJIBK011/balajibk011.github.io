f1f271cefd5dbe21492db48227dc7eee20d56de6
BALAJIBK011 <balajibk011@gmail.com>
2026-09-06 16:33:04 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,6 +1,6 @@
 // Copy your Firebase Web App configuration here.
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyBx05FSW7ml1IMj92j89jTV4xeRkC2xB24",
   authDomain: "balaji-faculty-portal.firebaseapp.com",
   projectId: "balaji-faculty-portal",
