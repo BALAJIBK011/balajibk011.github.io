b26777b9f21a0112aae32e4b4ba45296daad9e1f
BALAJIBK011 <balajibk011@gmail.com>
2026-09-06 18:15:43 +0530

    Delete js directory

--- js/cloudinary-config.js
+++
@@ -1,5 +0,0 @@
-// Cloudinary browser-upload configuration.
-// These are intentionally non-secret client-side values.
-export const CLOUDINARY_CLOUD_NAME = "al0uzxbq";
-export const CLOUDINARY_UPLOAD_PRESET = "balaji_faculty_upload";
-export const CLOUDINARY_FOLDER = "balaji-faculty";
