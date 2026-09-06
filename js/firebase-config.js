7a76707f49b34d20d8fd7845fac440d278a143d0
BALAJIBK011 <balajibk011@gmail.com>
2026-09-06 16:24:32 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,13 @@
 // Copy your Firebase Web App configuration here.
-// IMPORTANT: This client configuration is not a password/secret. Security is enforced by Firebase Authentication + Firestore/Storage Rules.
-export const firebaseConfig = {
-  apiKey: "PASTE_FIREBASE_API_KEY",
-  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "PASTE_SENDER_ID",
-  appId: "PASTE_APP_ID"
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBx05FSW7ml1IMj92j89jTV4xeRkC2xB24",
+  authDomain: "balaji-faculty-portal.firebaseapp.com",
+  projectId: "balaji-faculty-portal",
+  storageBucket: "balaji-faculty-portal.firebasestorage.app",
+  messagingSenderId: "98197536038",
+  appId: "1:98197536038:web:b8a9168eb8bc7b09b955e7",
+  measurementId: "G-DRBQ7XB7VQ"
 };
 
 // Use the email account you will create as the ONLY administrator.
